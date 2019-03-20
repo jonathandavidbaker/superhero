830251bb5562b253a07392b138f715e72206af9a
Jon Baker <jon.david.baker@gmail.com>
2019-03-20 10:04:20 -0500

    Created UI Layer

---
+++ SuperHero.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SuperHero.UI.MvcApplication" Language="C#" %>
---
+++ SuperHero.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuperHero.UI.Startup))]
+namespace SuperHero.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
