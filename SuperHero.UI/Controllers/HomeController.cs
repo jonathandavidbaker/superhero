5aa0c271916c91f74b8bbbb04eaadab9eac417ab
Jon Baker <jon.david.baker@gmail.com>
2019-03-21 09:53:30 -0500

    Made Contact page functional

--- SuperHero.UI/Controllers/HomeController.cs
+++ SuperHero.UI/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using SuperHero.UI.Models;
+using System.Web.Mvc;
+using System.Net.Mail;
+using System.Net;
+using System;
 
 namespace IdentitySample.Controllers
 {
@@ -22,9 +26,31 @@ namespace IdentitySample.Controllers
         [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (ModelState.IsValid)
+            {
+                string body = $"SuperHeroesInce Message from {cvm.Name} at {cvm.Email}: <br>{cvm.Message}";
+                MailMessage m = new MailMessage("no-reply@jdbaker.net", "jon.david.baker@gmail.com", cvm.Subject, body);
+                m.IsBodyHtml = true;
+                m.ReplyToList.Add(cvm.Email);
+
+                SmtpClient client = new SmtpClient("mail.jdbaker.net");
+                client.Credentials = new NetworkCredential("no-reply@jdbaker.net", "pr$aM*Y*38V");
+
+                try
+                {
+                    client.Send(m);     
+                }
+                catch (Exception e)
+                {
+                    ViewBag.Message = e.StackTrace;
+                }
+            }
+            return View("EmailConfirmation");
         }
     }
 }
---
+++ SuperHero.UI/Models/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHero.UI.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+    }
+}
